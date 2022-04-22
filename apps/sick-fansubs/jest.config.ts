31a779c3b73abbcb1b4b25cd1c1b41b6f1ec7a7d
Panos Louk <panosmagic32@yahoo.com>
2022-04-22 10:20:48 +0300

    Migrate to NX v14 & update dependencies

--- apps/sick-fansubs/jest.config.ts
+++ apps/sick-fansubs/jest.config.ts
@@ -1,6 +1,6 @@
 module.exports = {
-  displayName: 'shared',
-  preset: '../../jest.preset.js',
+  displayName: 'sick-fansubs',
+
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
     'ts-jest': {
@@ -8,7 +8,7 @@ module.exports = {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/libs/shared',
+  coverageDirectory: '../../coverage/apps/sick-fansubs',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
@@ -18,4 +18,5 @@ module.exports = {
     'jest-preset-angular/build/serializers/ng-snapshot',
     'jest-preset-angular/build/serializers/html-comment',
   ],
+  preset: '../../jest.preset.ts',
 };
--- libs/client/shell/jest.config.ts
+++ libs/client/shell/jest.config.ts
@@ -1,6 +1,6 @@
 module.exports = {
-  displayName: 'sick-fansubs',
-  preset: '../../jest.preset.js',
+  displayName: 'client-shell',
+
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
     'ts-jest': {
@@ -8,7 +8,7 @@ module.exports = {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/apps/sick-fansubs',
+  coverageDirectory: '../../../coverage/libs/client/shell',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
@@ -18,4 +18,5 @@ module.exports = {
     'jest-preset-angular/build/serializers/ng-snapshot',
     'jest-preset-angular/build/serializers/html-comment',
   ],
+  preset: '../../../jest.preset.ts',
 };
