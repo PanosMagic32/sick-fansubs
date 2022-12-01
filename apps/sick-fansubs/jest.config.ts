2aa5701e6428c488cd16f8c7b5f68d811e5436fd
Panos Louk <panosmagic32@yahoo.com>
2022-12-01 02:27:07 +0200

    style: format apps/ libs/ tools/ with prettierrc config

--- apps/sick-fansubs/jest.config.ts
+++ apps/sick-fansubs/jest.config.ts
@@ -1,23 +1,23 @@
 /* eslint-disable */
 export default {
-    displayName: 'sick-fansubs',
+  displayName: 'sick-fansubs',
 
-    setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-    globals: {
-        'ts-jest': {
-            tsconfig: '<rootDir>/tsconfig.spec.json',
-            stringifyContentPathRegex: '\\.(html|svg)$',
-        },
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
     },
-    coverageDirectory: '../../coverage/apps/sick-fansubs',
-    transform: {
-        '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
-    },
-    transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
-    snapshotSerializers: [
-        'jest-preset-angular/build/serializers/no-ng-attributes',
-        'jest-preset-angular/build/serializers/ng-snapshot',
-        'jest-preset-angular/build/serializers/html-comment',
-    ],
-    preset: '../../jest.preset.js',
+  },
+  coverageDirectory: '../../coverage/apps/sick-fansubs',
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+  preset: '../../jest.preset.js',
 };
