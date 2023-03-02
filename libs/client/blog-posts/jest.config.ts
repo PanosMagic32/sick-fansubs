89c027db99d589f14507a96e4f98a68b7c4be602
Panos Louk <panosmagic32@yahoo.com>
2023-03-02 12:28:10 +0200

    chore: update NX, run migrations & update dependencies

--- libs/client/blog-posts/jest.config.ts
+++ libs/client/blog-posts/jest.config.ts
@@ -3,15 +3,16 @@ export default {
   displayName: 'client-blog-posts',
   preset: '../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-      stringifyContentPathRegex: '\\.(html|svg)$',
-    },
-  },
+  globals: {},
   coverageDirectory: '../../../coverage/libs/client/blog-posts',
   transform: {
-    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
   },
   transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
   snapshotSerializers: [
