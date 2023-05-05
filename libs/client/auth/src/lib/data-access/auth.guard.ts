0f75c09950639dcaafab8410f097fa04d0715aff
Panos Louk <panosmagic32@yahoo.com>
2023-05-05 10:12:02 +0300

    chore: upgrade NX, run migrations & update dependencies

--- libs/client/auth/src/lib/data-access/auth.guard.ts
+++ libs/client/auth/src/lib/data-access/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router, UrlTree } from '@angular/router';
+import { Router, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
 import { TokenService } from './token.service';
@@ -7,7 +7,7 @@ import { TokenService } from './token.service';
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard {
   constructor(private router: Router, private tokenService: TokenService) {}
 
   canActivate(): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
