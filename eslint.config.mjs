a55d55607da299216744953b360726a74828e75c
Panos Louk <panosmagic32@yahoo.com>
2026-06-17 15:46:52 +0300

    chore: apply scope linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,45 +1,59 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nx from "@nx/eslint-plugin";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nx from '@nx/eslint-plugin';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default defineConfig([globalIgnores([
-    "**/node_modules/**",
-    "**/dist/**",
-    "**/coverage/**",
-    "**/.nx/**",
-]), {
+export default defineConfig([
+  globalIgnores(['**/node_modules/**', '**/dist/**', '**/coverage/**', '**/.nx/**']),
+  {
     plugins: {
-        "@nx": nx,
+      '@nx': nx,
     },
-}, {
-    files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
 
     rules: {
-        "@nx/enforce-module-boundaries": ["error", {
-            enforceBuildableLibDependency: true,
-            allow: [],
+      '@nx/enforce-module-boundaries': [
+        'error',
+        {
+          enforceBuildableLibDependency: false,
+          allow: [],
 
-            depConstraints: [{
-                sourceTag: "*",
-                onlyDependOnLibsWithTags: ["*"],
-            }],
-        }],
+          depConstraints: [
+            {
+              sourceTag: 'scope:shared',
+              onlyDependOnLibsWithTags: ['scope:shared'],
+            },
+            {
+              sourceTag: 'scope:api',
+              onlyDependOnLibsWithTags: ['scope:api', 'scope:shared'],
+            },
+            {
+              sourceTag: 'scope:web',
+              onlyDependOnLibsWithTags: ['scope:web', 'scope:shared'],
+            },
+          ],
+        },
+      ],
     },
-}, {
-    files: ["**/*.ts", "**/*.tsx"],
-    extends: compat.extends("plugin:@nx/typescript"),
-}, {
-    files: ["**/*.js", "**/*.jsx"],
-    extends: compat.extends("plugin:@nx/javascript"),
-}]);
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    extends: compat.extends('plugin:@nx/typescript'),
+  },
+  {
+    files: ['**/*.js', '**/*.jsx'],
+    extends: compat.extends('plugin:@nx/javascript'),
+  },
+]);
